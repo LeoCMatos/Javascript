e7f62ad22eaff07a84936b86831a3cbcec042c49
Leonardo Matos <leocamatos@outlook.com>
2023-05-17 22:29:20 -0300

    commit

---
+++ aula11/ex008.js
@@ -0,0 +1,10 @@
+
+var vel = 61
+console.log(`A velocidade do seu carro ${vel}Km`)
+
+if(vel > 60){
+    console.log(`Voce ultrapassou a velocidade permitida. Multado!`)
+} else {
+    console.log('Você está dentro dos limites de velocidade da via')
+}
+console.log(`Dirija sempre usando cinto de segurança!`)
